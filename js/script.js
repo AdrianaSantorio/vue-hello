01d7e3f786db249dc79791ee153c8c039539c3ed
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-26 17:54:10 +0100

    first draft

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('Vue ok', Vue);
+
+const root = new Vue (
+    {
+        el: '#root', 
+        data: {
+            helloText: 'Hello World!',
+            media: 'https://picsum.photos/200/300',
+        }
+    } 
+);
